772d2df318c84855d6ecb24b8dacbaaedf63f24a
namratanakhale <namratanakhale91@gmail.com>
2022-12-24 16:38:05 +0530

    useState removed

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,31 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const[input, setInput] = React.useState("")
-  const[inputNum, setInputNum] = React.useState("")
-  const handleInputText = (event) =>{
+  // const[input, setInput] = React.useState("")
+  // const[inputNum, setInputNum] = React.useState("")
+  const handleInput = (event) =>{
    // use console.log
-   setInput(event.target.value)
-   console.log(event.target.value)
+  //  setInput(event.target.value)
+  console.log(`Input in #${event.target.id} is ${event.target.value}`)
   }
 
-  const handleInputNum = (event) =>{
-    // use console.log
-    setInputNum(event.target.value)
-    console.log(event.target.value)
-   }
+  // const handleInputNum = (event) =>{
+  //   // use console.log
+  //   setInputNum(event.target.value)
+  //   console.log(event.target.value)
+  //  }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" onChange={handleInputText} type={'text'} />
+      <input id="text-input" onChange={handleInput} type={'text'} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input" onChange={handleInputNum} type={'number'} />
+      <input id="num-input" onChange={handleInput} type={'number'} />
       <br/>
     </div>
   )
